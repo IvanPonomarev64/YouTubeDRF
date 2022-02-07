ab04d2b83efb5737ca45e826a8c2802205a899d1
ivan <ivan_ponomarew@mail.ru>
2022-02-07 17:26:59 +0400

    преобразование из словаря в байтовую json строку

--- drfsite/women/serialazers.py
+++ drfsite/women/serialazers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+
 from .models import *
 
 
-class WomenSerialazer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+class WomenModel:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+
+class WomenSerialazer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+
+
+def encode():
+    model = WomenModel('Bibi', 'Cool')
+    model_sr = WomenSerialazer(model)
+    print(model_sr.data, type(model_sr.data), sep='\n')
+    json = JSONRenderer().render(model_sr.data)  # преобразуем в байтовую json строку
+    print(json)

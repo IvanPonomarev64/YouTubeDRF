a0c0c8006036558199e3b6edb70bc04e98df4693
ivan <ivan_ponomarew@mail.ru>
2022-02-11 16:00:19 +0400

    работа с роутерами

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,15 +1,27 @@
 from django.forms import model_to_dict
 from rest_framework import generics, viewsets
 from django.shortcuts import render
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from women.models import Women
+from women.models import Women, Category
 from women.serialazers import WomenSerializer
 
 
 class WomenViewSet(viewsets.ModelViewSet):
-    queryset = Women.objects.all()
+    # queryset = Women.objects.all()
     serializer_class = WomenSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        if pk:
+            return Women.objects.filter(pk=pk)
+        return Women.objects.all()
+
+    @action(methods=['get'], detail=False)
+    def category(self, request):
+        cats = Category.objects.all()
+        return Response({'cats': [c.name for c in cats]})  # http://127.0.0.1:8000/api/v1/women/category/
 
 
 # class WomenAPIList(generics.ListCreateAPIView):
